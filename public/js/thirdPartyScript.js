7cce1b9f0c200756b1c051c7d7155643a2c8f13e
redneran <t-narender@talasecurity.io>
2019-09-23 15:12:19 +0530

    changew IP

--- public/js/thirdPartyScript.js
+++ public/js/thirdPartyScript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   var ccn1 = $("#ccn1").text();
   var ccn2 = $("#ccn2").text();
   $.post({
-    url: 'http://172.29.16.4:4000/post-data',
+    url: 'http://192.168.128.36:4000/post-data',
     data: {
       ccn1: ccn1,
       ccn2: ccn2
