a69f61d96e27a031d8d8a6161d23b94aae6b44ed
Narender Kumar <nkk.snat@gmail.com>
2020-10-01 19:31:03 +0530

    Create ph-storage.js

---
+++ public/js/ph-storage.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  if (typeof (Storage) !== "undefined") {
+     localStorage.setItem("AMEX", "378282246311115");
+     sessionStorage.setItem("MASTER", "5555555555554444");
+   }
+});
